aa65dd3b5da8e8b823920db1f9208cb7f44e3ac0
Elsie Horne <elsiehorne@gmail.com>
2021-01-04 09:01:59 +0000

    finish allergy drugs

--- scripts/allergy_prescriptions.R
+++ scripts/allergy_prescriptions.R
@@ -128,16 +128,144 @@ allergy_drugs <- bind_rows(oral_antihistamines_v2 %>%
                              bind_rows(v3_codes %>%
                                          filter(nasal_allergy_drug) %>%
                                          select(read_code, read_term)) %>%
-                                         mutate(cat2 = 'nasal allergy drug'),
+                                         mutate(cat2 = 'nasal allergy drugs'),
                            eye_allergy_drugs_v2 %>%
                              bind_rows(v3_codes %>%
                                          filter(eye_allergy_drug) %>%
                                          select(read_code, read_term)) %>%
-                                         mutate(cat2 = 'eye allergy drug'),
+                                         mutate(cat2 = 'eye allergy drugs'),
                            topical_antihistamines_v2 %>%
                              bind_rows(v3_codes %>%
                                          filter(topical_antihistamine) %>%
                                          select(read_code, read_term)) %>%
-                                         mutate(cat2 = 'topical antihistamines')) %>%
-  mutate(cat1 = 'allergy drugs')
+                                         mutate(cat2 = 'topical antihistamines'))
   
+#### eczema #### 
+#  this list was constructed as follows (as recommened by susannah)
+# all 'realted drugs' here https://bnf.nice.org.uk/treatment-summary/eczema.html
+# all topical corticosteroids listed here https://bnf.nice.org.uk/treatment-summary/topical-corticosteroids.html
+eczema_bnf <- read_delim("lists_in/Elsie/eczema_bnf.txt", 
+                                     "\t", escape_double = FALSE,
+                                     trim_ws = TRUE, col_names = FALSE) %>%
+  mutate_all(tolower) %>%
+  distinct() %>%
+  arrange(X1)
+# search in OpenSafely (only include topical preparations)
+
+eczema_os <- read_delim("lists_in/Elsie/elsie-opensafely-paste-eczema", 
+                         "\t", escape_double = FALSE,
+                         trim_ws = TRUE, col_names = FALSE) 
+
+# check for any I forgot to expand
+eczema_os %>%
+  filter(str_detect(X1, '⊞'))
+
+eczema_os <- eczema_os %>%
+  mutate_all(list(~ str_remove(., '\\W+'))) %>%
+  filter(X1!='') 
+
+# for some reason, the opening bracket is missing for some of the samples,
+# so extract Read code as follows
+read_code <- str_remove(str_extract(eczema_os$X1, '.{6}$'), '\\)')
+read_term <- sapply(seq_along(eczema_os$X1), function(x) str_remove(eczema_os$X1[x], read_code[x]))
+read_term <- str_remove(str_remove(read_term, '\\)$'), '\\($')  
+
+eczema <- tibble(read_code = read_code, read_term = read_term)
+
+eczema_v2 <- read_delim("lists_in/Elsie/eczema_psoriasis_preps_v2", 
+                                     "\t", escape_double = FALSE,
+                                     trim_ws = TRUE, col_names = FALSE) %>%
+  select(read_code = X1, read_term = X3)
+
+eczema_v2 %>%
+  left_join(eczema, by = 'read_code') %>%
+  print(n=Inf)
+
+# add
+# m51i.
+# BUFEXAMAC yes m52..
+# TACROLIMUS yes m5E..
+# PINE TAR yes m5G..
+# DITHRANOL no (psoriasis only)
+# ETRETINATE no (psoriasis only)
+# SALICYLIC ACID no
+# GAMOLENIC ACID no
+# CALCIPOTRIOL no
+# ACITRETIN no
+# TACALCITOL no
+# TAZAROTENE no
+
+eczema_v2 <- eczema_v2 %>%
+  filter(read_code %in% 'm51i.' |
+         str_detect(read_code, '^m52')|
+         str_detect(read_code, '^m5E')|
+         str_detect(read_code, '^m55'))
+
+eczema <- eczema %>%
+  bind_rows(eczema_v2) %>%
+  mutate(cat2 = 'eczema preparations')
+
+
+#### adrenaline ####
+adrenaline_pens <- read_csv("lists_in/OpenSafely/opensafely-adrenaline-pens-1964af81.csv") %>%
+  select(term = dmd_name)
+adrenaline_auto <- read_csv("lists_in/OpenSafely/opensafely-adrenaline-auto-injectors-327422b6.csv") %>%
+  select(term)
+
+adrenaline_os <- bind_rows(adrenaline_pens, adrenaline_auto) %>%
+  distinct() %>%
+  arrange(term)
+
+adrenaline_v3 <- read_csv("lists_in/Elsie/elsie-horne-adrenaline-024148b3.csv") %>%
+  rename_all(~ str_c('read_',.))
+
+adrenaline_v2 <- read_delim("lists_in/Elsie/adrenaline_v2", 
+                        "\t", escape_double = FALSE,
+                        trim_ws = TRUE, col_names = FALSE) %>%
+  select(read_code = X1, read_term = X3) %>%
+  # remove mini-jet products
+  filter(!str_detect(read_term, 'MIN.+I-JET'))
+
+adrenaline <- bind_rows(adrenaline_v2, adrenaline_v3) %>%
+  distinct(read_code, .keep_all = TRUE) %>%
+  mutate(cat2 = 'adrenaline')
+
+####
+
+allergy_drugs <- allergy_drugs %>%
+  bind_rows(eczema, adrenaline) %>%
+  mutate(cat1 = 'allergy prescriptions') %>%
+  mutate_at('read_term', list(~ str_remove(tolower(.), '^\\*'))) %>%
+  distinct()
+
+####
+
+write_csv(allergy_drugs, path = 'lists_out/allergy_prescriptions.csv')
+
+# latex tables
+allergies_list <- allergy_drugs %>%
+  group_split(cat2)
+
+allergies_latex_table <- function(.data) {
+  category <- .data %>%
+    distinct(cat2) %>% 
+    unlist() %>% 
+    unname()
+  
+  category_ <- str_to_lower(str_replace_all(category, ' ', '_'))
+  
+  caption <- str_c('Read codes for \\emph{\'', category, '\'} (return to \\nameref{cha:ehr:methods:pre:allergy} methods)')
+  label <- str_c('tab:app:rc_', category_)
+  
+  table <- .data %>%
+    arrange(read_term) %>%
+    select(`Read code` = read_code, 
+           `Term` = read_term) %>%
+    xtable(caption = caption,
+           label = label,
+           align=c('l',"p{2cm}","p{10cm}")) %>%
+    print_xtable_multi(filename = category_)
+  
+}
+
+lapply(allergies_list, allergies_latex_table)
